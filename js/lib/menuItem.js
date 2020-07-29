3f5bbe3e20d8cfd9d86c59d3d998cb1c9780a827
Mithil Verma <mithilve@gmail.com>
2020-07-29 15:39:23 +0530

    TAE1

--- js/lib/menuItem.js
+++ js/lib/menuItem.js
@@ -1,56 +1,49 @@
+function menuItem(image) {
+  if (typeof image != "string") {
+    throw "Constructor argument must be text";
+  }
 
-function menuItem( image )
-{
-    if ( typeof(image) != 'string' )
-    {
-        throw 'Constructor argument must be text';
+  var _image = gamejs.image.load(image);
+  var _size = _image.getSize();
+  var _active = false;
+  var _callback = null;
+
+  this.setActive = function (active) {
+    if (typeof active != "boolean") {
+      throw "Active flag must be boolean";
     }
 
-    var _image    = gamejs.image.load(image);
-    var _size     = _image.getSize();
-    var _active   = false;
-    var _callback = null;
+    var rect = new gamejs.Rect([0, 0], [parseInt(_size[0] / 2), _size[1]]);
 
-    this.setActive = function( active ){
-        if ( typeof(active) != 'boolean' )
-        {
-            throw 'Active flag must be boolean';
-        }
-
-        var rect = new gamejs.Rect([0,0],[parseInt(_size[0] / 2), _size[1]]);
-
-        if ( active )
-        {
-            rect.x = parseInt(_size[0] / 2);
-        }
-
-        _image.crop(rect);
-        _active = active;
-
-        return this;
+    if (active) {
+      rect.x = parseInt(_size[0] / 2);
     }
 
-    this.addCallback = function( callback ){
-        if ( typeof(callback) !== 'function' )
-        {
-            throw 'Callback must be a function';
-        }
+    _image.crop(rect);
+    _active = active;
 
-        _callback = callback;
-        return this;
+    return this;
+  };
+
+  this.addCallback = function (callback) {
+    if (typeof callback !== "function") {
+      throw "Callback must be a function";
     }
 
-    this.setText = function( text ){
-        if ( typeof(text) != 'string' )
-        {
-            throw 'Text argument must be text';
-        }
+    _callback = callback;
+    return this;
+  };
 
-        _text = text;
-        return this;
+  this.setText = function (text) {
+    if (typeof text != "string") {
+      throw "Text argument must be text";
     }
 
-    this.getCanvas = function(){
-        return _image;
-    }
+    _text = text;
+    return this;
+  };
+
+  this.getCanvas = function () {
+    return _image;
+  };
 }
--- js/main.js
+++ js/main.js
@@ -1,75 +1,83 @@
-gamejs = require('gamejs');
-font   = require('gamejs/font');
+gamejs = require("gamejs");
+font = require("gamejs/font");
 
 //Preload all the required images
 gamejs.preload([
-    'img/splash-screen.png','img/new-game.png', 'img/bg.png','img/player.png',
-    'img/blank.png','img/switch.png','img/door.png','img/goal.png',
-    'img/platform-left.png','img/platform-right.png',
-    'img/platform-middle.png', 'img/selected.png',
-    'img/scorecard-background.png','img/game-window.png',
-    'img/reset.png', 'img/star-off.png', 'img/star-on.png',
-    'img/menu-button.png', 'img/next-button.png', 'img/end.png'
+  "img/splash-screen.png",
+  "img/new-game.png",
+  "img/bg.png",
+  "img/player.png",
+  "img/blank.png",
+  "img/switch.png",
+  "img/door.png",
+  "img/goal.png",
+  "img/platform-left.png",
+  "img/platform-right.png",
+  "img/platform-middle.png",
+  "img/selected.png",
+  "img/scorecard-background.png",
+  "img/game-window.png",
+  "img/reset.png",
+  "img/star-off.png",
+  "img/star-on.png",
+  "img/menu-button.png",
+  "img/next-button.png",
+  "img/end.png",
 ]);
 
-gamejs.ready(function() {
+gamejs.ready(function () {
+  var display = gamejs.display.setMode([800, 600]);
 
-    var display = gamejs.display.setMode([800, 600]);
+  //Ensure that all required files are included
+  include_once(["lib/startMenu.js"]);
 
-    //Ensure that all required files are included
-    include_once([
-        'lib/startMenu.js'
-    ]);
+  var mainSurface = gamejs.display.getSurface();
+  var mainWindow = new startMenu();
+  var self = this;
 
-    var mainSurface = gamejs.display.getSurface();
-    var mainWindow  = new startMenu();
-    var self        = this;
+  // msDuration = time since last tick() call
+  var tick = function (msDuration) {
+    mainSurface.fill("#FFFFFF");
 
-    // msDuration = time since last tick() call
-    var tick = function(msDuration){
-        mainSurface.fill("#FFFFFF");
+    //Handle user input
+    mainWindow.handleInput(mainSurface);
 
-        //Handle user input
-        mainWindow.handleInput( mainSurface );
+    //Update the worlds objects
+    mainWindow.update(msDuration);
 
-        //Update the worlds objects
-        mainWindow.update( msDuration );
+    //Draw the new objects
+    mainWindow.draw(mainSurface);
+  };
 
-        //Draw the new objects
-        mainWindow.draw( mainSurface );
-    };
+  //Set up listeners for the body when a scorecard is present
+  $("body").keydown(function (event) {
+    //Only check key presses if the scorcard is shown
+    if ($("#game_scorecard").is(":visible")) {
+      switch (event.keyCode) {
+        //Enter and e will all move to the next level
+        case 13:
+        case 69:
+          $("#game_scorecard .nextLevel").click();
+          event.preventDefault();
+          break;
+        //Escape will quit to the menu
+        case 27:
+          $("#game_scorecard .mainMenu").click();
+          event.preventDefault();
+          break;
+        //r will reset the level
+        case 82:
+          $("#game_scorecard .resetLevel").click();
+          event.preventDefault();
+          break;
+      }
+    }
+  });
 
-    //Set up listeners for the body when a scorecard is present
-    $('body').keydown(function(event){
-        //Only check key presses if the scorcard is shown
-        if ( $('#game_scorecard').is(":visible") )
-        {
-            switch ( event.keyCode )
-            {
-                //Enter and e will all move to the next level
-                case 13:
-                case 69:
-                    $('#game_scorecard .nextLevel').click();
-                    event.preventDefault();
-                    break;
-                //Escape will quit to the menu
-                case 27:
-                    $('#game_scorecard .mainMenu').click();
-                    event.preventDefault();
-                    break;
-                //r will reset the level
-                case 82:
-                    $('#game_scorecard .resetLevel').click();
-                    event.preventDefault();
-                    break;
-            }
-        }
-    });
+  //Remove the loading bar
+  $("#preload").remove();
+  $("#gameWindow").show();
 
-    //Remove the loading bar
-    $('#preload').remove();
-    $('#gameWindow').show();
-
-    //Set up the tick function, run at 60fps
-    gamejs.time.fpsCallback(tick, self, 60);
+  //Set up the tick function, run at 60fps
+  gamejs.time.fpsCallback(tick, self, 60);
 });
